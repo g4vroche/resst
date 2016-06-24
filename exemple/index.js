81feff420aeca7a313ca809c2d90392c7d74e3f6
Hugues Charleux <hcx@uhuru.io>
2016-06-24 10:22:17 +0200

    Updated messy exemple

--- exemple/index.js
+++ exemple/index.js
@@ -1,37 +1,53 @@
 var Client = require("../src/index.js");
-var backend = require("../src/jquery-backend.js");
+var backend = require("../src/resst-jquery.js");
 
 
 var client = new Client({
-    "host": "http://localhost:9000/api",
+    "host": "http://localhost:8080/api",
     "backend": backend,
     "hooks": {
-        "before": {
-            "all": [accessToken],
-            "POST": [getCSRFToken],
-            "PUT": [getCSRFToken],
-            "PATCH": [getCSRFToken],
-            "DELETE": [getCSRFToken],
-        }
+        "before": [accessToken, getCSRFToken],
+        "after": [consoleLog1, consoleLog2]
     }
 });
 
-function accessToken(request, next){
-    request.headers["Access-Token"] = "yolo";
+
+function consoleLog1(transaction, next){
+    console.log("ConsoleLog1 Function")
+    console.log(transaction.response)
+    console.log("-------------------")
     return next();
 }
 
-function getCSRFToken(request, next){
-    return client.get("/security/token")
-          .then(function(response) {
 
-            request.body += "&_csrf="+response.data.value; 
-            return next();
+function consoleLog2(transaction, next){
+    console.log("ConsoleLog2 Function")
+
+    console.log("-------------------")
+    return next();
+}
+
+function accessToken(transaction, next){
+    transaction.request.headers["Access-Token"] = "yolo";
+    return next();
+}
+
+function getCSRFToken(transaction, next){
+
+    if(transaction.request.method === "GET"){
+        return next();
+    }
+
+    return client.get("/security/token")
+      .then(function(subTransaction) {
+        console.log(subTransaction);
+        transaction.request.body += "&_csrf="+subTransaction.response.data.value; 
+        return next();
     });
 }
 
 
-var foo = client.get("/users/42").then(console.log, console.warn);
+var foo = client.get("/users/42").then( (data) => console.log(data));
 
 
 
